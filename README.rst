84ad2a66a60fbf3545650b005f9afea966dd22e7
Laurent Lasudry <lasudry@gmail.com>
2017-03-01 15:07:48 +0100

    Initial commit

---
+++ README.rst
@@ -0,0 +1,23 @@
+===============
+affinitic.theme
+===============
+
+This is a Diazo theme package.
+
+
+License
+-------
+
+The project is licensed under the GPLv2.
+
+
+Credits
+-------
+
+This package was developed by `Affinitic team <https://github.com/affinitic>`_.
+
+.. image:: http://www.affinitic.be/affinitic_logo.png
+   :alt: Affinitic website
+   :target: http://www.affinitic.be
+
+``affinitic.theme`` is licensed under GNU General Public License, version 2.
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+version = 1.0
+
+setup(name='affinitic.theme',
+      version=version,
+      description='Affinitic Theme',
+      long_description=open('README.rst').read() + '\n' +
+                       open('CHANGES.rst').read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Plone',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+      keywords='affinitic diazo theme',
+      author='Affinitic',
+      author_email='info@affinitic.be',
+      url='https://github.com/affinitic/affinitic.theme',
+      license='GPL',
+      packages=find_packages('src', exclude=['ez_setup']),
+      package_dir={'': 'src'},
+      namespace_packages=['affinitic'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.themingplugins',
+          # -*- Extra requirements: -*-
+      ])
