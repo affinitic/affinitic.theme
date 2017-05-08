37005315acd32163bb48ac86e350aeb090eb97e9
AuroreMariscal <aurore@affinitic.be>
2017-05-08 09:15:36 +0200

    Add style css and animation
    This refs affinitic #6481

--- src/affinitic/theme/static/js/animation.js
+++ src/affinitic/theme/static/js/animation.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 //    Parallax
 //    var lastScrollTop = 0;
 //    $( window ).scroll(function() {
-//        var st = $(this).scrollTop();
+//        var st = $(this).scrollLeft();
 //        if (st > lastScrollTop){
 //            scroll = "0";
 //        } else {
@@ -20,12 +20,12 @@ $(document).ready(function(){
 //        }
 //        lastScrollTop = st;
 
-//        var scrolled = $(window).scrollTop();
-//        document.getElementById('portal-header').style.top = scrolled / 2 + "px";
+//        var scrolled = $(window).scrollLeft();
+//        document.getElementById('portal-header').style.left = scrolled / 2 + "px";
 //    });
 
 
-    $('.subsection-equipe').each(function() {
+    $('.template-zaffolderview').each(function() {
         $window.scroll(function(){
             $('#content-core').find('article').each(function() {
                 class_el = get_style_no_view($(this));
@@ -58,4 +58,56 @@ $(document).ready(function(){
             return 'no-view-right'
         }
     }
+
+
+//    ACCUEIL
+    $('#slides_home').each(function() {
+        nbr_slide = $(this).find('article').size();
+        width_slides_home = nbr_slide * 100 + '%';
+        width_article_home = 100 / nbr_slide + '%';
+        $('#slides_home').css('width', width_slides_home);
+        $(this).append( '<div id="slides_button_home"></div>' );
+        i=0;
+        $(this).find('article').each(function() {
+            i++;
+            id = 'button_' + i;
+            $(this).attr('id', id)
+            $(this).css('float', 'left');
+            $(this).css('width', width_article_home);
+            $('#slides_button_home').append( '<a href="#' + id + '" class="button_home"><div id="button_home"></div></a>' );
+        });
+    });
+    $('.button_home').click(function(){
+        var id = $(this).attr("href");
+        var offset = $(id).offset().left
+        $('html, body').animate({scrollLeft: offset}, 'slow');
+        return false;
+    });
+
+
+    function position_scroll(el) {
+        el_offset_left = el.offset().left;
+        quart = el.outerWidth() / 4;
+        el_offset_right = el_offset_left + el.outerWidth();
+        position = el_offset_left + 1;
+
+        var docScroll = $window.scrollLeft();
+        if( (docScroll > (el_offset_left - quart) ) && (docScroll < (el_offset_right - quart  )) ){
+            $('html, body').animate({scrollLeft: position}, 'slow');
+            return false;
+        }
+    }
+
+//    $( window ).scroll(function() {
+//        clearTimeout( $.data( this, "scrollCheck" ) );
+//        $.data( this, "scrollCheck", setTimeout(function() {
+//            $('#slides_home').find('article').each(function() {
+//                position_scroll($(this));
+//            });
+//        }, 500) );
+//        return false;
+//    });
+
+
+
 });
