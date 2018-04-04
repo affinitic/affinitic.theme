491a03402a9f81aa06101b7d7a9a73c8e938c12b
AuroreMariscal <aurore@affinitic.be>
2018-04-04 10:54:59 +0200

    Adapt style
    This refs affinitic ##

--- src/affinitic/theme/static/js/animation.js
+++ src/affinitic/theme/static/js/animation.js
@@ -5,8 +5,8 @@
  * Distributed under terms of the LICENCE.txt license.
  */
 jQuery(document).ready(function() {
-    $("#lightSlider").lightSlider({
-        item: 3,
+    $("#lightSlider_team").lightSlider({
+        item: 1,
         autoWidth: false,
         slideMove: 1, // slidemove will be 1 if loop is true
         slideMargin: 10,
@@ -17,11 +17,62 @@ jQuery(document).ready(function() {
         cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
         easing: 'linear', //'for jquery animation',////
 
-        speed: 400, //ms'
+        speed: 1000, //ms'
         auto: true,
         loop: true,
         slideEndAnimation: true,
-        pause: 2000,
+        pause: 4000,
+
+        keyPress: false,
+        controls: true,
+        prevHtml: '',
+        nextHtml: '',
+
+        rtl:false,
+        adaptiveHeight:false,
+
+        vertical:false,
+        verticalHeight:500,
+        vThumbWidth:100,
+
+        thumbItem:10,
+        pager: true,
+        gallery: false,
+        galleryMargin: 5,
+        thumbMargin: 5,
+        currentPagerPosition: 'middle',
+
+        enableTouch:true,
+        enableDrag:true,
+        freeMove:true,
+        swipeThreshold: 40,
+
+        responsive : [],
+
+        onBeforeStart: function (el) {},
+        onSliderLoad: function (el) {},
+        onBeforeSlide: function (el) {},
+        onAfterSlide: function (el) {},
+        onBeforeNextSlide: function (el) {},
+        onBeforePrevSlide: function (el) {}
+    });
+    $("#slider_tools").lightSlider({
+        item: 5,
+        autoWidth: false,
+        slideMove: 1, // slidemove will be 1 if loop is true
+        slideMargin: 10,
+
+        addClass: '',
+        mode: "slide",
+        useCSS: true,
+        cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
+        easing: 'linear', //'for jquery animation',////
+
+        speed: 2000, //ms'
+        auto: true,
+        loop: true,
+        slideEndAnimation: true,
+        pause: 3000,
 
         keyPress: false,
         controls: true,
