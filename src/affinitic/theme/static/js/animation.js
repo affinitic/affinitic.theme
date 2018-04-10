89028ada586176f3c02c244e001d5ec1aeb28fa6
AuroreMariscal <aurore@affinitic.be>
2018-04-10 20:22:55 +0200

    Add style
    This refs affinitic ##

--- src/affinitic/theme/static/js/animation.js
+++ src/affinitic/theme/static/js/animation.js
@@ -107,4 +107,55 @@ jQuery(document).ready(function() {
         onBeforeNextSlide: function (el) {},
         onBeforePrevSlide: function (el) {}
     });
+    $("#slider_team_folder").lightSlider({
+        item: 1,
+        autoWidth: false,
+        slideMove: 1, // slidemove will be 1 if loop is true
+        slideMargin: 10,
+
+        addClass: '',
+        mode: "slide",
+        useCSS: true,
+        cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
+        easing: 'linear', //'for jquery animation',////
+
+        speed: 2000, //ms'
+        auto: true,
+        loop: true,
+        slideEndAnimation: true,
+        pause: 3000,
+
+        keyPress: false,
+        controls: true,
+        prevHtml: '',
+        nextHtml: '',
+
+        rtl:false,
+        adaptiveHeight:true,
+
+        vertical:false,
+        verticalHeight:500,
+        vThumbWidth:100,
+
+        thumbItem:12,
+        pager: true,
+        gallery: true,
+        galleryMargin: 5,
+        thumbMargin: 5,
+        currentPagerPosition: 'middle',
+
+        enableTouch:true,
+        enableDrag:true,
+        freeMove:true,
+        swipeThreshold: 40,
+
+        responsive : [],
+
+        onBeforeStart: function (el) {},
+        onSliderLoad: function (el) {},
+        onBeforeSlide: function (el) {},
+        onAfterSlide: function (el) {},
+        onBeforeNextSlide: function (el) {},
+        onBeforePrevSlide: function (el) {}
+    });
 });
